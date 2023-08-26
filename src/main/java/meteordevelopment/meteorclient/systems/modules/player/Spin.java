e2184b1fdd10ae9a8e3d32a23732b5f59105f4cf
slimakiswinka <81806308+slimakiswinka@users.noreply.github.com>
2023-08-26 19:27:37 +0200

    Update Spin.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/Spin.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/Spin.java
@@ -3,6 +3,7 @@ package meteordevelopment.meteorclient.systems.modules.player;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.DoubleSetting;
 import meteordevelopment.meteorclient.settings.EnumSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
@@ -11,7 +12,15 @@ import meteordevelopment.orbit.EventHandler;
 
 
 public class Spin extends Module {
-
+    private final SettingGroup speed = settings.createGroup("Speed");
+    private final Setting<Integer> Speed = speed.add(new IntSetting.Builder()
+        .name("speed")
+        .description("Speed of Rotation.")
+        .defaultValue(20)
+        .range(-1000, 1000)
+        .sliderRange(-100, 100)
+        .build()
+    );
     public Spin() {
         super(Categories.Player, "im-only-human", "Im only human after all.");
     }
@@ -23,7 +32,7 @@ public class Spin extends Module {
 
     @EventHandler
     private void onTick(TickEvent.Post event) {
-        currentYaw += 2.0f;
+        currentYaw += (Speed.get().floatValue() / 10f);
 
         if (currentYaw >= 360.0f) {
             currentYaw = 0.0f;
